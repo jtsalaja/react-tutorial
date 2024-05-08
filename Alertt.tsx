8db021c3c9e9cce846bb92dec1f8953debca3c4b
Jelizaveta Tsalaja <jelizaveta.tsalaja@gmail.com>
2024-05-08 19:11:30 -0400

    Update Alert.tsx

--- Alertt.tsx
+++ Alertt.tsx
@@ -1,11 +1,18 @@
-import { ReactNode } from "react";
+import { ReactNode } from "react"
 
 interface Props {
-  children: ReactNode;
+    children: ReactNode;
+    onClose: () => void;
 }
 
-const Alert = ({ children }: Props) => {
-  return <div className="alert alert-primary">{children}</div>;
-};
+const Alert = ({children, onClose} : Props) => {
+  return (
+    <div className="alert alert-primary alert-dismissible">
+        {children}
+    <button type="button" className="btn-close" onClick={onClose} data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
 
-export default Alert;
+  )
+}
+
+export default Alert
